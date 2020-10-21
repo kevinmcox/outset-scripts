370c33eaccd90ad2656533fb1a7fbef2b25c1e27
Kevin M. Cox <kevinmcox@users.noreply.github.com>
2020-10-21 11:31:17 -0500

    Desktop Picture 2.0
    Updated to use the excellent Desktoppr tool instead of AppleScript.

--- usr/local/outset/login-once/DesktopPicture.sh
+++ usr/local/outset/login-once/DesktopPicture.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/zsh
 
-## Version 1.0
+## Version 2.0
 ## Compiled by Kevin M. Cox
 
 ## Sets the initial desktop picture for all users at first login
-## Should be set in Munki to REQUIRE the custom DesktopPictures.pkg
+## Should be set in Munki to REQUIRE the custom DesktopPictures.pkg, Outset and Desktoppr
 
 # Set the user's desktop picture as The Daily News 175th Anniversary logo
 
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/GalvNews 175 Years.png"'
+/usr/local/bin/desktoppr "/Library/Desktop Pictures/GalvNews 175 Years.png"
