afde22e5a89310cbbda6bb399922f380e7114467
Kevin M. Cox <kevinmcox@users.noreply.github.com>
2020-11-08 16:35:43 -0600

    Dock Once 1.3

--- usr/local/outset/login-once/DockOnce.sh
+++ usr/local/outset/login-once/DockOnce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## Version 1.2 - Mojave
+## Version 1.3 - Catalina
 ## Compiled by Kevin M. Cox
 
 ## Uses dockutil to setup the user's dock the first time they login
@@ -8,11 +8,12 @@
 
 ## Remove unneeded default icons
 
-/usr/local/bin/dockutil --remove 'Siri' --no-restart
 /usr/local/bin/dockutil --remove 'Launchpad' --no-restart
-/usr/local/bin/dockutil --remove 'Photos' --no-restart
-/usr/local/bin/dockutil --remove 'Messages' --no-restart
 /usr/local/bin/dockutil --remove 'FaceTime' --no-restart
+/usr/local/bin/dockutil --remove 'Messages' --no-restart
+/usr/local/bin/dockutil --remove 'Photos' --no-restart
+/usr/local/bin/dockutil --remove 'Music' --no-restart
+/usr/local/bin/dockutil --remove 'Podcasts' --no-restart
+/usr/local/bin/dockutil --remove 'TV' --no-restart
 /usr/local/bin/dockutil --remove 'News' --no-restart
-/usr/local/bin/dockutil --remove 'iTunes' --no-restart
 /usr/local/bin/dockutil --remove 'App Store'
