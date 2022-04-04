070f9ad8d21ecda2135ca8e6865f66fc1a33db02
Kevin M. Cox <kevinmcox@users.noreply.github.com>
2022-04-04 14:32:12 -0500

    Mail Settings 1.1
    The location of the Mail preference file changed starting with macOS 11 Big Sur.

--- usr/local/outset/login-once/MailSettings.sh
+++ usr/local/outset/login-once/MailSettings.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
-## Version 1.0
+## Version 1.1
 ## Compiled by Kevin M. Cox
 
 ## Sets the initial Mail settings for users the first time they login
@@ -8,8 +8,8 @@
 
 # Show duplicate messages
 
-/usr/bin/defaults write com.apple.mail _AlwaysShowDuplicates -bool true
+/usr/bin/defaults write ~/Library/Containers/com.apple.mail/Data/Library/Preferences/com.apple.mail.plist _AlwaysShowDuplicates -bool true
 
 # Only take address@example.com when copying email addresses
 
-/usr/bin/defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+/usr/bin/defaults write ~/Library/Containers/com.apple.mail/Data/Library/Preferences/com.apple.mail.plist AddressesIncludeNameOnPasteboard -bool false
